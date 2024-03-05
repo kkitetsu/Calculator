d9f9024fa4974483436710e326aa6d025857af68
kkitetsu <b787kitetsu@gmail.com>
2024-03-05 17:00:18 +0900

    Fix mul div order

--- src/main/java/controller/calculatorController.java
+++ src/main/java/controller/calculatorController.java
@@ -1,6 +1,9 @@
 package controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -41,37 +44,72 @@ public class calculatorController extends HttpServlet {
 		// TODO Auto-generated method stub
 		String heading = request.getParameter("label");
 		System.out.println(heading);
-		String[] arrayCal = heading.split(" ");
+		List<String> arrayCal = new ArrayList<>(Arrays.asList(heading.split(" ")));
 		int toReturn = 0;
-		toReturn = calculateHelper(arrayCal);
-//		try {
-//			toReturn = calculate(arrayCal);
-//			toReturn = calculateHelper(arrayCal);
-//		} catch (Exception e) {
-//			toReturn = Integer.MIN_VALUE;
-//		}
+		try {
+			arrayCal = removeMulDiv(arrayCal);
+			toReturn = cal(arrayCal);
+		} catch (Exception e) {
+			toReturn = Integer.MIN_VALUE;
+		}
 		request.setAttribute("result", toReturn);	
 		doGet(request, response);
 	}
 	
-	public int calculateHelper(String[] str) {
-		if (str.length == 3) {
-			return calculate(str);
+	public List<String> removeMulDiv(List<String> str) {
+		while (true) {
+			if (str.contains("*")) {
+				// idx = str.index("*")
+				int idx = str.indexOf("*");
+				
+				// tmp = calculate(str[idx-1:idx+2])
+				int tmp = calculate(str.subList(idx - 1, idx + 2));
+				
+				str.remove(idx - 1); // str.pop(idx-1)
+				str.remove(idx - 1); // str.pop(idx-1)
+				
+				// str[idx - 1] = tmp
+				str.set(idx - 1, Integer.toString(tmp));
+				
+			} else if (str.contains("/")) {
+				// idx = str.index("*")
+				int idx = str.indexOf("/"); 
+				
+				// tmp = calculate(str[idx-1:idx+2])
+				int tmp = calculate(str.subList(idx - 1, idx + 2)); 
+				
+				str.remove(idx - 1); // str.pop(idx-1)
+				str.remove(idx - 1); // str.pop(idx-1)
+				
+				// str[idx - 1] = tmp
+				str.set(idx - 1, Integer.toString(tmp));
+			} else {
+				break;
+			}
 		}
-		String[] slicedArray = new String[str.length - 2]; // なんたる使いにくい言語だ
-		System.arraycopy(str, 0, slicedArray, 0, str.length - 2);
-		return calculateHelper(slicedArray) + Integer.parseInt(str[str.length - 1]);
+		return str;
 	}
 	
-	public int calculate(String[] str) {
-		if (str[1].equals("+")) {
-			return Integer.parseInt(str[0]) + Integer.parseInt(str[2]);
-		} else if (str[1].equals("-")) {
-			return Integer.parseInt(str[0]) - Integer.parseInt(str[2]);
-		} else if (str[1].equals("*")) {
-			return Integer.parseInt(str[0]) * Integer.parseInt(str[2]);
+	public int cal(List<String> str) {
+		int N = str.size();
+		for (int i = 0; i < N - 2; i += 2) {
+			// str[i+2] = calculate(str[i:i+3])
+			str.set(i + 2, Integer.toString(calculate(str.subList(i, i + 3))));
+		}
+		// return str[N-1]
+		return Integer.parseInt(str.get(N - 1)); // Return the last index
+	}
+	
+	public int calculate(List<String> str) {
+		assert(str.size() == 3);
+		if (str.get(1).equals("+")) {
+			return Integer.parseInt(str.get(0)) + Integer.parseInt(str.get(2));
+		} else if (str.get(1).equals("-")) {
+			return Integer.parseInt(str.get(0)) - Integer.parseInt(str.get(2));
+		} else if (str.get(1).equals("*")) {
+			return Integer.parseInt(str.get(0)) * Integer.parseInt(str.get(2));
 		} else {
-			return Integer.parseInt(str[0]) / Integer.parseInt(str[2]);
+			return Integer.parseInt(str.get(0)) / Integer.parseInt(str.get(2));
 		}
 	}
 
---
+++ src/test/java/controller/controller/calculatorControllerTests.java
@@ -0,0 +1,17 @@
+package controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class calculatorControllerTests {
+
+	@Test
+	void test() {
+		int test = 2;
+		assert(test == 2);
+		assert(test == 3);
+	}
+	
+
+}
